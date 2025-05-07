19194ea4452d25955d6fb9d6affe9ae4e9301f17
snikmahf <168553376+snikmahf@users.noreply.github.com>
2025-05-07 11:12:31 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "60X5UjKHMNp":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+
+}
+
